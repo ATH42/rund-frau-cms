6c9fe52ecd57df58a31e278378c7a86542b812d2
Fred <frederik.koenig@posteo.de>
2025-05-20 13:56:04 +0200

    added intro to index

--- schemaTypes/index.ts
+++ schemaTypes/index.ts
@@ -1,5 +1,6 @@
 import contactReasons from './ContactForm'
 import course from './Course'
+import intro from './intro'
 import roomImage from './RoomImage'
 import schedule from './Schedule'
 import services from './Services'
@@ -7,6 +8,7 @@ import teamImage from './TeamImage'
 import teamMembers from './TeamMembers'
 
 export const schemaTypes = [
+  intro,
   contactReasons,
   course,
   roomImage,
