e68b351484ba54ef588cbbaae683df3712a6710d
ATH <alex.t.hoffmann@icloud.com>
2025-05-26 12:00:11 +0200

    chore(config): add hostname

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -3,8 +3,9 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: 'x8r4dlu1',
-    dataset: 'production'
+    dataset: 'production',
   },
+  studioHost: 'rund-frau',
   /**
    * Enable auto-updates for studios.
    * Learn more at https://www.sanity.io/docs/cli#auto-updates
